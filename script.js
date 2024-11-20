2e77f2be88aa5b8b9442863c719eb20af89c934c
MrOty1 <oskaras.venzlauskas@stud.kitm.lt>
2024-11-20 20:30:34 +0200

    Add files via upload
    Added files

---
+++ script.js
@@ -0,0 +1,7 @@
+while(true){
+    let passcode = prompt("What is the code?")
+    if (passcode == "69420"){
+        break;
+    }
+}
+document.body.style.background = "#f3f3f3 url('images/hidden/background.gif')";
